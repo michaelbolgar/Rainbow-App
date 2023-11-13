4cdf8d5048c003205ede3bbcaa9e7c8b43740014
Liliya (Tacarlen) <l.feodotova296@yandex.ru>
2023-11-13 20:59:03 +0300

    added animation to the game screen

---
+++ Rainbow-App/Rainbow-App/Moduls/Game Screen/View/ColorsPatternView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorsPatternView.swift
+//  Rainbow-App
+//
+//  Created by Лилия Феодотова on 13.11.2023.
+//
+
+import UIKit
+
+class ColorsPatternView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
