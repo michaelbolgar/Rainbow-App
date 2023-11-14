266cf153a7ed1b532f778ba865360d3488b7f575
ShirobokovNikita <shirobokov97@yandex.ru>
2023-11-14 21:22:02 +0400

    for request

--- Rainbow-App/Rainbow-App/Moduls/Main Screen/View/MainView.swift
+++ Rainbow-App/Rainbow-App/Moduls/Main Screen/View/MainView.swift
@@ -21,12 +21,8 @@ class MainView: UIView {
         self.backgroundColor = Palette.orange
         setupRainbow()
         setupLayout()
-        
-        circleView.backgroundColor = Palette.red
-        circleView.layer.cornerRadius = 25
-        circleView.clipsToBounds = true // обрезание текста
-        circleView.snp.makeConstraints { make in
-                    make.width.height.equalTo(50) // Ширина и высота для круга
+        characterCircle.snp.makeConstraints { make in
+                    make.width.height.equalTo(40) // Ширина и высота для круга
                 }
         
         horizontalStack.axis = .horizontal
@@ -40,18 +36,15 @@ class MainView: UIView {
     // MARK: Methods
     
     private func setupRainbow() {
-//        for i in 0...colors.count-1 {
-//            circleView.backgroundColor = colors[i]
-//            characterCircle.text = rainbowWord[i]
-//            characterCircle.textColor = .white
-//            circleView.addSubview(characterCircle)
-//            horizontalStack.addArrangedSubview(circleView)
-//        }
-        circleView.backgroundColor = colors[0]
-        characterCircle.text = rainbowWord[0]
-        characterCircle.textColor = .white
-        circleView.addSubview(characterCircle)
-        horizontalStack.addArrangedSubview(circleView)
+        for i in 0...colors.count-1 {
+            characterCircle.text = rainbowWord[i]
+            characterCircle.textColor = .white
+            characterCircle.backgroundColor = colors[i]
+            characterCircle.layer.cornerRadius = 20
+            characterCircle.textAlignment = .center
+            characterCircle.clipsToBounds = true
+            horizontalStack.addArrangedSubview(characterCircle)
+        }
     }
 
     private func setupLayout() {
