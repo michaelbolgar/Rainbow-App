ed895b2c1e187ece2028d710fc66d66e997f740c
michaelbolgar <mr.mbolgar@inbox.ru>
2023-11-17 18:58:38 +0100

    added settings screen

--- Rainbow-App/Rainbow-App/Moduls/Settings Screen/View/SettingsView.swift
+++ Rainbow-App/Rainbow-App/Moduls/Settings Screen/View/SettingsView.swift
@@ -4,11 +4,25 @@ import SnapKit
 
 class SettingsView: UIView {
 
-    // MARK: Init
+    lazy var settingsTableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 80
+        tableView.isScrollEnabled = true
+        tableView.register(SettingsViewCell.self, forCellReuseIdentifier: SettingsViewCell.identifier)
+        return tableView
+    }()
 
+    // MARK: Init
     override init (frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = Palette.purple
+        self.backgroundColor = Palette.backgroundBlue
+
+        self.addSubview(settingsTableView)
+        settingsTableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(20)
+        }
     }
 
     required init?(coder: NSCoder) {
